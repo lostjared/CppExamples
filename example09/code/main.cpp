2a3833e1400e1db99d53fbeb2c168d1f868ba3b7
lostjared <jaredbruni@gmail.com>
2018-03-28 08:28:08 -0700

    updated source file

--- example09/code/main.cpp
+++ example09/code/main.cpp
@@ -1,6 +1,27 @@
 #include<iostream>
+#include"ac.h"
 
 int main(int argc, char **argv) {
-
+    cv::VideoCapture cap(0);
+    if(!cap.isOpened()) {
+        std::cerr << "Error could not open webcam.\n";
+        exit(EXIT_FAILURE);
+    }
+    bool active = true;
+    cv::namedWindow("AC");
+    std::cout << "press lowercase q to quit!\n";
+    std::cout << "value of filter 15: " << ac::draw_strings[15] << "\n";
+    while(active) {
+        cv::Mat m;
+        if(!cap.read(m)) {
+            std::cerr << "End of file..\n";
+            exit(EXIT_SUCCESS);
+        }
+        ac::Bitwise_XOR(m); // filter from ac.h
+        cv::imshow("AC", m);
+        if(cv::waitKey(10) == 'q') {
+            exit(EXIT_SUCCESS);
+        }
+    }
 	return 0;
 }
